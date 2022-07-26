3e54be1b699f1fa3f2f3866febf5262b790a94a3
Charles Santos <thyarles@gmail.com>
2022-07-26 19:19:04 -0300

    First code

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo $IN_CYPRESS
+echo $VTEX_ACCOUNT
+echo $VTEX_APP_KEY
+echo $VTEX_APP_TOKEN
+echo $VTEX_WORKSPACE
+echo $VTEX_TOOLBELT_GIT
+echo $VTEX_TOOLBELT_BRANCH
