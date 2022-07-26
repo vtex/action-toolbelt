0ef8e2cf43aa9acbf584198c11c604fc1bfde12f
Charles Santos <thyarles@gmail.com>
2022-07-26 20:58:56 -0300

    Fix branch on output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-VERSION="$(vtex-e2e --version) [from https://github.com/$VTEX_TOOLBELT_GIT/tree/$VTEX_VTEX_TOOLBELT_BRANCH]"
+VERSION="$(vtex-e2e --version) [from https://github.com/$VTEX_TOOLBELT_GIT/tree/$VTEX_TOOLBELT_BRANCH]"
 echo ::notice title=Toolbelt version used::$VERSION
